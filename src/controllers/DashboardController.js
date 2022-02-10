4ed61592277afc20de9cd664255a4c54d0a43e8b
tql247 <tqlinh.exe@gmail.com>
2022-02-10 12:11:04 +0700

    dashboard

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,15 +1,32 @@
-const Extension = require("../utils/Extension")
-const AccountService = require("../services/AccountService")
+const Extension = require("../utils/Extension");
+const AccountService = require("../services/AccountService");
+const FarmService = require("../services/FarmService");
+const SensorService = require("../services/SensorService");
+const MachineService = require("../services/MachineService");
+const RuleService = require("../services/RuleService");
 
 class AccountController {
     // Trả về giao diện dashboard
     async get(req, res, next) {
         try {
-            return res.render('_layout', { page: 'dashboard'})
+            const accounts = await AccountService.getAll();
+            const farms = await FarmService.getAll();
+            const sensors = await SensorService.getAll();
+            const machines = await MachineService.getAll();
+            const rules = await RuleService.getAll();
+
+            return res.render("_layout", {
+                page: "dashboard",
+                numAccount: accounts.length,
+                numFarm: farms.length,
+                numSensor: sensors.length,
+                numMachine: machines.length,
+                numRule: rules.length,
+            });
         } catch (error) {
-            next(error)
+            next(error);
         }
     }
 }
 
-module.exports = new AccountController()
+module.exports = new AccountController();
